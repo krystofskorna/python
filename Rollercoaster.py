08815795156dab83ffce1d1143417064a384649d
krystofskorna <156819910+krystofskorna@users.noreply.github.com>
2024-01-28 21:13:28 +0100

    Rename Rollercoaster to Rollercoaster.py
     + Doplnění informací a komentářů.

--- Rollercoaster.py
+++ Rollercoaster.py
@@ -1,14 +1,24 @@
 # Horská dráha
 
+# Popis programu:
+        # Na začátku program vypíše větu tam zadáš výšku v cm 
+        # Odpoví vám jestli můžete jet na zdráze
+        # Následuje otázka ohledně věku kvůli ceně lístku 
+        # Potom otázka na vyfocení při jízdě
+
+# Uvítací věta
 print("Vítejte na horské dráze ")
+
+# Uživatelské vstupy
 height = int(input("Jaká je vaše výška v cm?\n"))
 bill = 0
 
+# Podmínky a sčítání
 if height >= 87 :
     print("Můžete jet na horské dráze.")
     
     age = int(input("Jaký je váš věk?\n"))
-    
+       
     if age <= 12 :
         bill += 50
         print("Cena vašeho lístku je 50 Kč")
@@ -22,7 +32,8 @@ if height >= 87 :
     else :
         bill += 150
         print("Cena vašeho lístku je 150 Kč")
-
+    
+    # Fotka při jízdě
     photo = input("Chcete během jízdy vyfotit?  ano nebo ne\n")
 
     if photo == "ano" :
@@ -33,3 +44,5 @@ if height >= 87 :
 
 else :
     print("Omlouváme se , ale na horské dráze jet nemůžete")
+
+# Ukázka použití: python Rollercoaster.py
