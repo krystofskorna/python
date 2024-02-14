864bf88d499cdb6ed786a070b93a85d7ae3abb3c
krystofskorna <156819910+krystofskorna@users.noreply.github.com>
2024-02-14 15:13:03 +0100

    Create EdgeDetect.py

---
+++ EdgeDetect.py
@@ -0,0 +1,27 @@
+# Detekce okrajů
+
+import cv2
+
+# inicializace objektů pro stream z kamery
+video = cv2.VideoCapture(0)
+
+while True:
+    # načtení snímku z kamery
+    success, img = video.read()
+    if success == True: # overeni uspesneho ziskani snimku z kamery
+        # prevedení snímku do stupňu šedi
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # pužití algoritmu Canny pro detekci hran
+        edges = cv2.Canny(gray, 80, 160)
+        # Zobrazeni vysledku
+        cv2.imshow('Demo - Edge Detection', edges)
+        cv2.imshow('Demo - Grey senzor', gray)
+        cv2.imshow('Demo - Colorful senzor', img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+# uvolnění prostředků a ukončení programu
+video.release()
+cv2.destroyAllWindows()
